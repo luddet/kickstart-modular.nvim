9fd922c3786cb21f00d242b897b7c57ae006fc65
luddet <ludde@tunel.se>
2024-03-20 19:49:49 +0100

    Added neotree and some options and keymaps

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {}
+    vim.keymap.set({ 'n', 'v' }, '<leader>fe', ':Neotree filesystem reveal toggle<cr>', { desc = 'Toggle NeoTree' })
+  end,
+}
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -20,10 +20,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -47,5 +47,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- Back to normal mode
+vim.keymap.set('i', 'kj', '<Esc><Esc>', { desc = 'Exit insert mode' })
 
 -- vim: ts=2 sts=2 sw=2 et
